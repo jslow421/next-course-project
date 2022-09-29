2367bdadc52f534c2ac61458ed2d600027e478d7
John Slowik <john@jslowik.com>
2022-09-28 19:52:43 -0500

    GetStaticProps example

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,5 +1,24 @@
-export default function ProductsPage() {
+import {GetStaticProps} from "next";
+
+export interface ProductModel {
+    id: string;
+    title: string;
+}
+
+export default function ProductsPage(props: {products: ProductModel[]}) {
     return (
-        <p>Products</p>
+        <ul>
+            {props.products.map((product) => (
+                <li key={product.id}>{product.title}</li>
+            ))}
+        </ul>
     );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+    return {
+        props: {
+            products: [{id: "p1", title: "Product 1"}, {id: "p2", title: "Product 2"}],
+        },
+    };
+};
